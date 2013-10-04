c39f90f6d19050517b5bbdee2a740ca11ae2e6d5
tunaslut <tunaslut@gmail.com>
2013-10-04 18:14:21 +0000

    directives.js edited online with Bitbucket

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,12 +4,12 @@
 angular.module('myApp.directives', []).
   directive('pulseAnimate', [function() {
     return function(scope, elm, attrs) {
-         scope.$watch(attrs.pulseAnimate, function() {
+         /*scope.$watch(attrs.pulseAnimate, function() {
             elm.animate({
                 width:290,
                 height:290,
             },50).animate({width:300, height:300},50);
 
-        });
+        });*/
     };
   }]);
