4244b973f84a502b37f223ca7f9bbf27bf5feca9
poissonpie <poissonpie@gmail.com>
2013-10-07 08:37:27 +0000

    move some files around

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngCookies', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']);
+angular.module('myApp', ['ngCookies', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+    config(['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/game', {templateUrl: 'partials/game.html', controller: 'GhostController'});
+            $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
+            $routeProvider.otherwise({redirectTo: '/game'});
+            }]);
